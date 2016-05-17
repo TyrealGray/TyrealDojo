6034e400201c12b610d06958f2e90117608c1396
tyrealgray <tyrealgray@ymail.com>
2016-05-17 17:03:01 +0800

    move mosaic dojo into second dir

--- nodeWebBasic.js
+++ nodeWebBasic.js
@@ -1,36 +1,43 @@
 var PORT = 3000;
 
 var http = require('http');
-var url=require('url');
-var fs=require('fs');
-var path=require('path');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var dir = path.dirname(fs.realpathSync(__filename));
 
 var localFileType = {
-  "css": "text/css",
-  "gif": "image/gif",
-  "html": "text/html",
-  "ico": "image/x-icon",
-  "jpeg": "image/jpeg",
-  "jpg": "image/jpeg",
-  "js": "text/javascript",
-  "json": "application/json",
-  "pdf": "application/pdf",
-  "png": "image/png",
-  "svg": "image/svg+xml",
-  "swf": "application/x-shockwave-flash",
-  "tiff": "image/tiff",
-  "txt": "text/plain",
-  "wav": "audio/x-wav",
-  "wma": "audio/x-ms-wma",
-  "wmv": "video/x-ms-wmv",
-  "xml": "text/xml"
+    "css": "text/css",
+    "gif": "image/gif",
+    "html": "text/html",
+    "ico": "image/x-icon",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "js": "text/javascript",
+    "json": "application/json",
+    "pdf": "application/pdf",
+    "png": "image/png",
+    "svg": "image/svg+xml",
+    "swf": "application/x-shockwave-flash",
+    "tiff": "image/tiff",
+    "txt": "text/plain",
+    "wav": "audio/x-wav",
+    "wma": "audio/x-ms-wma",
+    "wmv": "video/x-ms-wmv",
+    "xml": "text/xml"
 };
 
 var server = http.createServer(function (request, response) {
     var pathname = url.parse(request.url).pathname;
-    
+
     console.log("Request for " + pathname + " received.");
-    
+
+    if (pathname == '/') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(dir + '/index.html').pipe(response);
+        return;
+    }
+
     var ext = path.extname(pathname);
     ext = ext ? ext.slice(1) : 'unknown';
     fs.exists(pathname.substr(1), function (exists) {
