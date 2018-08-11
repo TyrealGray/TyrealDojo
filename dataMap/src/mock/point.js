44f2fffdb551d0be470932c2f5c537be57548240
TyrealGray <tyrealgray@ymail.com>
2018-08-12 01:11:07 +0200

    add comment

--- dataMap/src/mock/point.js
+++ dataMap/src/mock/point.js
@@ -1,3 +1,4 @@
+//https://launchprocess-dot-manage-sv.appspot.com/stats/near/55.98452698569709/3.42256558309532
 export default {
 	text:'{"latitude":55.984526985697094,"longitude":-3.42256558309532,"heading":-100.3047103881836,"link":"https://www.google.com/maps/@55.984527,-3.4225656,0a,75y,-100.3h,89.01t/pointsDataSource\u003d!3m4!1e1!3m2!1sAF1QipPIcWwY5n7KvC3sd3KXglcufSl9gNVWZtmq6dyD!2e10","trip":3}'
 }
